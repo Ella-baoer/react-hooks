1d4836d8c77c70bb1df7344c68434ba0365e05d7
Jiao Zeng <vivienne.ze2018@gmail.com>
2023-12-08 17:15:31 +0800

    useEffect changes

--- src/components/useEffectCom.js
+++ src/components/useEffectCom.js
@@ -1,37 +1,36 @@
-import { render } from "@testing-library/react"
-import React, {Fragment, useState, useEffect} from "react"
-import { Button } from "react-bootstrap"
+import React, { Fragment, useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
 
 const UseEffectCom = () => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const handleResize = () => {
-    setWindowWidth(window.innerWidth)
-  }
+    setWindowWidth(window.innerWidth);
+  };
   useEffect(() => {
-    window.addEventListener('resize', handleResize)
+    window.addEventListener("resize", handleResize);
     return () => {
       // clean up
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
-  // const [resourceType, setResourceType] = useState('posts')
-  // const [items, setItems] = useState([])
-  // useEffect(() => {
-  //   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-  //     .then(response => response.json())
-  //     .then(json => setItems(json))
-  // }, [resourceType])
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const [resourceType, setResourceType] = useState('posts')
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }, [resourceType])
   return (
     <Fragment>
-      <div>{windowWidth}
-        {/* <Button onClick={() => {setResourceType('posts')}}>Posts</Button>
+      <div>
+        {windowWidth}
+        <Button onClick={() => {setResourceType('posts')}}>Posts</Button>
         <Button onClick={() => {setResourceType('users')}}>Users</Button>
-        <Button onClick={() => {setResourceType('comments')}}>Comments</Button> */}
+        <Button onClick={() => {setResourceType('comments')}}>Comments</Button>
       </div>
-      {/* <h1>{resourceType}</h1>
-      {items.map((item) => <pre>{JSON.stringify(item)}</pre>)} */}
+      <h1>{resourceType}</h1>
+      {/* {items.map((item) => <pre>{JSON.stringify(item)}</pre>)} */}
     </Fragment>
-  )
-}
+  );
+};
 
 export default UseEffectCom;
